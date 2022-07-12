9dcc099e54bb576d7bda5072ef4efd60f969aef8
TheEE145 <Arhitector2009@gmail.com>
2022-07-12 18:26:29 +0300

    functions and variables in sessionVars

--- bin/run-handler.js
+++ bin/run-handler.js
@@ -2,16 +2,19 @@ const fs = require('fs');
 const err = require('./err');
 
 let
+    lastargOfFuncHandler,
+    arguments2,
     component,
     fragment,
+    package3,
     from,
+    func,
     as2;
 
 class RunHandler {
     static Reader = class {
         static class = function(dir, isSystem) {
             let as3 = {};
-            console.log(fs.readdirSync(dir));
             for(let package2 of fs.readdirSync(dir)) {
                 if(fs.lstatSync(`${dir}\\${package2}`).isDirectory()) {
                     as3[package2] = RunHandler.Reader.class(`${dir}\\${package2}`, isSystem);
@@ -24,6 +27,22 @@ class RunHandler {
 
             return as3;
         };
+
+        static trim = function(text) {
+            for(let variant of [' ', '\n', '\t', '\r']) {
+                if(text[0] == variant) {
+                    text = text.substring(1);
+                    return RunHandler.Reader.trim(text);
+                };
+            };
+
+            return text;
+        };
+
+        static unArgument = function(text) {
+            text = text.substring(1, text.length - 1);
+            
+        };
     };
 
     static Runtime = class {
@@ -35,7 +54,11 @@ class RunHandler {
 
             code = code.split(';');
             for(let line = 0; line < code.length; line++) {
-                fragment = code[line].split(' ');
+                fragment = RunHandler.Reader.trim(code[line]).split(' ');
+
+                if(fragment[0] == '') {
+                    continue;
+                };
 
                 if(fragment[0] == 'import') {
                     component = as2 = fragment[1];
@@ -61,12 +84,8 @@ class RunHandler {
                     if(from == 'prgl') {
                         component = `.\\modules\\${component}`;
 
-                        if(
-                            fs.existsSync(component) || fs.existsSync(component + '.js')
-                        ) {
-                            if(
-                                fs.lstatSync(component).isDirectory() || fs.lstatSync(component + 'js').isDirectory()
-                            ) {
+                        if(fs.existsSync(component) || fs.existsSync(component + '.js')) {
+                            if(fs.lstatSync(component).isDirectory()) {
                                 RunHandler.Runtime.sessionVars[as2] = RunHandler.Reader.class(component, true);
                             } else {
                                 RunHandler.Runtime.sessionVars[as2] = require(`../modules/${component}.js`);
@@ -78,9 +97,35 @@ class RunHandler {
 
                     continue;
                 };
-            };
 
-            console.log(RunHandler.Runtime.sessionVars);
+                fragment[0] = fragment[0].split('.');
+
+                lastargOfFuncHandler = fragment[0][fragment[0].length - 1];
+                for(let char of lastargOfFuncHandler) {
+                    if(char == '(') {
+                        break;
+                    };
+
+                    lastargOfFuncHandler = lastargOfFuncHandler.substring(1);
+                };
+                
+                fragment[0][fragment[0].length - 1] = fragment[0][fragment[0].length - 1].substring(0, 
+                    fragment[0][fragment[0].length - 1].length - lastargOfFuncHandler.length);
+
+                func = RunHandler.Runtime.sessionVars[fragment[0][0]];
+                for(let i = 1; i < fragment[0].length; i++) {
+                    package3 = fragment[0][i];
+                    func = func[package3];
+                };
+
+                if(typeof func === 'undefined') {
+                    err(`${fragment[0].join('.')} not defined`);
+                };
+
+                if(typeof func == 'function') {
+                    func(lastargOfFuncHandler);
+                };
+            };
         };
     };
 };
