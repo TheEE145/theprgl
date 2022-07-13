ab6d586a90dc16c0b0df843d581c8c9ad9e61521
TheEE145 <72571816+TheEE145@users.noreply.github.com>
2022-07-13 13:20:58 +0300

    clean

--- modules/System/Op/add.js
+++
@@ -1,6 +0,0 @@
-const RunHandler = require("../../../run-handler");
-
-module.exports = function(value, update) {
-    RunHandler.Runtime.sessionVars[value] = 
-        Math.floor(RunHandler.Runtime.sessionVars[value]) + update;
-};
